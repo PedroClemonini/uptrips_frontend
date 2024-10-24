30d1fb6c66108fefb76b476e90fdf78570dd10fc
Pedro Clemonini <pclemonini-00@hotmail.com>
2024-10-24 09:16:25 -0300

    feat:createuser

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import axios from "axios";
 import './App.css';
 
-function App() {
+const App = () => {
+ const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:8001/api/users", formData)
+
+      .then(response =>{
+        console.log(response.data);
+      })
+      setSuccessMessage("Cadastro realizado com sucesso!");
+      setErrorMessage(""); 
+    } catch (error) {
+      console.error('Erro ao consumir a API', error);
+      setErrorMessage("Erro ao realizar o cadastro. Tente novamente.");
+      setSuccessMessage("");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="cadastro-container">
+      <div className="cadastro-form">
+        <h1 className="cadastro-title">Cadastro</h1>
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+        {successMessage && <p className="success-message">{successMessage}</p>}
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="name"
+            className="cadastro-input"
+            placeholder="Nome"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            className="cadastro-input"
+            placeholder="Email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            className="cadastro-input"
+            placeholder="Senha"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+          <button type="submit" className="cadastro-button">Cadastrar</button>
+        </form>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
