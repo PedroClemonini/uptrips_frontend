bc92850cff629df209b2e2bfef263f01e04f5292
Pedro Clemonini <pclemonini-00@hotmail.com>
2024-11-07 19:06:30 -0300

    fix:first-auth

--- app/src/App.js
+++ app/src/App.js
@@ -1,79 +1,23 @@
-import React, { useState } from "react";
-import api from './Api.js'; 
-import './App.css';
-
+import React, { useEffect } from "react";
+import "./App.css";
+import EditPackage from "./EditarPacote.js";
+import Login from "./login.js";
+import InserirPacote from "./InserirPacote.js";
+import axios from "axios";
 const App = () => {
- const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    password: "",
-  });
+  useEffect(() => {
+    axios.defaults.withCredentials = true;
 
-  const [errorMessage, setErrorMessage] = useState("");
-  const [successMessage, setSuccessMessage] = useState("");
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try{
-     const response = await api.post("/users", formData);
-        console.log(response.data);
-        setSuccessMessage("Cadastro realizado com sucesso!");
-        setErrorMessage(""); 
-      
-    }catch (error) {
-      console.error('Erro ao consumir a API', error);
-      setErrorMessage("Erro ao realizar o cadastro. Tente novamente.");
-      setSuccessMessage("");
-    };
-  };
-
-  return (
-    <div className="cadastro-container">
-      <div className="cadastro-form">
-        <h1 className="cadastro-title">Cadastro</h1>
-        {errorMessage && <p className="error-message">{errorMessage}</p>}
-        {successMessage && <p className="success-message">{successMessage}</p>}
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="name"
-            className="cadastro-input"
-            placeholder="Nome"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
-          <input
-            type="email"
-            name="email"
-            className="cadastro-input"
-            placeholder="Email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-          <input
-            type="password"
-            name="password"
-            className="cadastro-input"
-            placeholder="Senha"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-          <button type="submit" className="cadastro-button">Cadastrar</button>
-        </form>
-      </div>
-    </div>
-  );
+    axios
+      .get("https://uptrips.api.simplifica.gru.br/sanctum/csrf-cookie")
+      .then((response) => {
+        console.log("Token CSRF obtido com sucesso");
+      })
+      .catch((error) => {
+        console.error("Erro ao obter o token CSRF:", error);
+      });
+  }, []);
+  return <Login />;
 };
 
 export default App;
