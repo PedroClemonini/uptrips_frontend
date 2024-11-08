f7d2b5ff235dc2849645bd1ad3d2b76dbe047941
Pedro Clemonini <pclemonini-00@hotmail.com>
2024-11-08 12:54:48 -0300

    fix: login moved

--- app/src/App.js
+++ app/src/App.js
@@ -1,20 +1,9 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import Login from "./login.js";
-import axios from "axios";
 const App = () => {
-  useEffect(() => {
-    axios.defaults.withCredentials = true;
-    axios
-      .get("https://uptrips.api.simplifica.gru.br/sanctum/csrf-cookie")
-      .then((response) => {
-        console.log("Token CSRF obtido com sucesso");
-      })
-      .catch((error) => {
-        console.error("Erro ao obter o token CSRF:", error);
-      });
-  }, []);
-  return <Login />;
+
+return <Login />;
 };
 
 export default App;
