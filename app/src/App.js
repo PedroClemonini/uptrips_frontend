49ea714d7b3157bd29801e7857248c79e283e350
matheusgoncalves <matheus.sil.goncalves@gmail.com>
2024-11-13 22:01:52 -0300

    componente-filtro

--- app/src/App.js
+++ app/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "react-router-dom";
 import { Route,Routes, BrowserRouter } from "react-router-dom";
 import ManagerView from "./components/ManagerView";
+import TripFilter from "./components/TripFilter";
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ManagerView />} />
+        <Route path="/teste" element={<TripFilter/>} />
       </Routes>
     </BrowserRouter>
   );
