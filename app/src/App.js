2a176d2bf7cdb7f7fe55378547dd240a85a06ff5
matheusgoncalves <matheus.sil.goncalves@gmail.com>
2024-11-14 13:15:47 -0300

    footer

--- app/src/App.js
+++ app/src/App.js
@@ -3,12 +3,15 @@ import "react-router-dom";
 import { Route,Routes, BrowserRouter } from "react-router-dom";
 import ManagerView from "./components/ManagerView";
 import TripFilter from "./components/TripFilter";
+import Footer from "./components/footer"
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ManagerView />} />
         <Route path="/teste" element={<TripFilter/>} />
+        <Route path="/teste2" element={<Footer/>} />
+
       </Routes>
     </BrowserRouter>
   );
