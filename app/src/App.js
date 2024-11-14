12ed0c76551ed1bbc8c67aa8a13a37edc71e668b
Pedro Clemonini <pclemonini-00@hotmail.com>
2024-11-14 00:46:42 -0300

    fix:login and register auth

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "react-router-dom";
-import { Route,Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import ManagerView from "./components/ManagerView";
+import Login from "./login";
+import Register from "./register";
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ManagerView />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
